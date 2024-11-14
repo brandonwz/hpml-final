6011a1a16c03bd79ca2c7f8e19cb1ddc8aeeeebe
brandonzhang <brandonwz6860@outlook.com>
2024-11-13 22:39:15 -0800

    update train.sh script and upload clippings

--- BitDistiller-Fork/train/train.sh
+++ BitDistiller-Fork/train/train.sh
@@ -1,4 +1,4 @@
-export MODEL_PATH='/root/WizardCoder-Python-7B/'
+export MODEL_PATH='/home/brandon/hpml/hpmlfinal/Llama-Guard-3-1B/' #CHANGE TO YOUR PATH
 export SAVE_PATH=$2
 export MASTER_ADDR="localhost"
 export MASTER_PORT="1321"
@@ -6,10 +6,10 @@ export GLOO_SOCKET_IFNAME="lo"
 export NCCL_SOCKET_IFNAME="lo"
 export WANDB_DISABLED=true  
 
-deepspeed --num_gpus=8 train.py \
+deepspeed --num_gpus=1 train.py \
     --model_name_or_path $MODEL_PATH \
     --data_path $1 \
-    --model_max_length 1024 \
+    --model_max_length 131072 \
     --output_dir $SAVE_PATH \
     --logging_dir $3 \
     --num_train_epochs $4 \
@@ -37,4 +37,10 @@ deepspeed --num_gpus=8 train.py \
     --train_kd True \
     --kd_loss_type "cakld" \
     --max_train_samples 999999 \
-    --clip BitDistiller/quantization/clip_cache/WizardCoder-7B/7b-int2-g128-twoclip.pt
+    --clip /home/brandon/hpml/hpmlfinal/BitDistiller/quantization/clip_cache/hf-llama3-1b/int2-g128.pt #CHANGE TO YOUR PATH WITH THE CLIPPINGS, INCLUDED IN quantization/clip_cache/hf-llama3-1b
+
+
+# data_path: the path to the generated toxic chat teacher dataset in data/generation/datasets/llama-guard-3-1b/toxicchat_<suffix>
+# SAVE_PATH: whatever you want
+# logging_dir: whatever you want
+# epochs: either 2 or 4, maybe 2 for now
